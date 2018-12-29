05e382742d5e951d5c3a5c64935dad8f3a2c07dd
Argembarger <45947601+Argembarger@users.noreply.github.com>
2018-12-29 15:03:09 -0500

    AutoArg Bugfix 4
    Class-ified

--- AutoArg.js
+++ AutoArg.js
@@ -1,78 +1,79 @@
 // AutoArg -- manually injected action triggering
 // After loading Trimps, paste the following code into your console:
 // (SetInterval is at the bottom and controls what actually happens)
-var autoArgMapAtZoneAndCell = new function(_zone, _cell) {
-  if(game.global.world == _zone && !game.global.preMapsActive && !game.global.mapsActive) {
-    if(game.global.lastClearedCell + 2 == _cell) {
-      mapsClicked();
-    }
-  }
-};
-
-var autoArgEssenceFarm = new function() {
-  // Uses S if there is essence on the map
-  if(!game.global.mapsActive && countRemainingEssenceDrops() > 0) {
-    setFormation('4'); // S
-    return true;
-  }
-  return false;
-};
-
-var autoArgStanceDance = new function() {
-  if(this.autoArgEssenceFarm() == true) return;
-  // Very basic stancedancing
-  else if(game.global.formation == 2 && game.global.soldierHealth <= game.global.soldierHealthMax * 0.125) {
-    setFormation('0');
-  }
-  else if(game.global.formation == 0 && game.global.soldierHealth <= game.global.soldierHealthMax * 0.125) {
-    setFormation('1')
-  }
-  else if(game.global.formation == 1 && game.global.soldierHealth == game.global.soldierHealthMax) {
-    setFormation('2');
-  }
-};
-
-var autoArgBoneFarm = new function() {
-  // If have been in map for 45 mins, go back to world.
-  if(((getGameTime() - game.global.zoneStarted) / 1000) > (45 * 60)) {		
-    // If in map and not switching to maps
-    if(game.global.mapsActive && !game.global.switchToMaps) {
-      mapsClicked();
-    }
-    // If on map screen and not switching to world
-    if(game.global.preMapsActive && !game.global.switchToMaps) {
-      mapsClicked();
-    }
-  }
-  else {
-    // If in world and fighting, go to map screen.
-    if(!game.global.preMapsActive && !game.global.mapsActive && game.global.fighting) {
-      this.autoArgMapAtZoneAndCell(game.global.world, 100);
-    }
-    // If on map screen, try to farm.
-    else if(game.global.preMapsActive && !game.global.mapsActive && !game.global.fighting) {
-      // Presets are used as priorities in this system.
-      // If can't buy preset 1, buy preset 2, else buy preset 3, else give up and farm.
-      let mapsOwnedBeforeFarming = game.global.mapsOwned;
-      selectAdvMapsPreset(1);
-      buyMap(); // try to buy with current settings;
-      if(game.global.mapsOwned == mapsOwnedBeforeFarming) {
-        selectAdvMahpsPreset(2);
-        buyMap();
-        if(game.global.mapsOwned == mapsOwnedBeforeFarming) {
-          selectAdvMahpsPreset(3);
-          buyMap();
+class AutoArg {
+	constructor() {
+    this.autoArgMapAtZoneAndCell = function(_zone, _cell) {
+      if(game.global.world == _zone && !game.global.preMapsActive && !game.global.mapsActive) {
+        if(game.global.lastClearedCell + 2 == _cell) {
+          mapsClicked();
         }
       }
-      selectMap('map' + game.global.mapsOwned); // Select latest map
-      runMap();
-    }
+    };
+    this.autoArgEssenceFarm = function() {
+      // Uses S if there is essence on the map
+      if(!game.global.mapsActive && countRemainingEssenceDrops() > 0) {
+        setFormation('4'); // S
+        return true;
+      }
+      return false;
+    };
+    this.autoArgStanceDance = function() {
+      if(this.autoArgEssenceFarm() == true) return;
+      // Very basic stancedancing
+      else if(game.global.formation == 2 && game.global.soldierHealth <= game.global.soldierHealthMax * 0.125) {
+        setFormation('0');
+      }
+      else if(game.global.formation == 0 && game.global.soldierHealth <= game.global.soldierHealthMax * 0.125) {
+        setFormation('1')
+      }
+      else if(game.global.formation == 1 && game.global.soldierHealth == game.global.soldierHealthMax) {
+        setFormation('2');
+      }
+    };
+    this.autoArgBoneFarm = new function() {
+      // If have been in map for 45 mins, go back to world.
+      if(((getGameTime() - game.global.zoneStarted) / 1000) > (45 * 60)) {		
+        // If in map and not switching to maps
+        if(game.global.mapsActive && !game.global.switchToMaps) {
+          mapsClicked();
+        }
+        // If on map screen and not switching to world
+        if(game.global.preMapsActive && !game.global.switchToMaps) {
+          mapsClicked();
+        }
+      }
+      else {
+        // If in world and fighting, go to map screen.
+        if(!game.global.preMapsActive && !game.global.mapsActive && game.global.fighting) {
+          this.autoArgMapAtZoneAndCell(game.global.world, 100);
+        }
+        // If on map screen, try to farm.
+        else if(game.global.preMapsActive && !game.global.mapsActive && !game.global.fighting) {
+          // Presets are used as priorities in this system.
+          // If can't buy preset 1, buy preset 2, else buy preset 3, else give up and farm.
+          let mapsOwnedBeforeFarming = game.global.mapsOwned;
+          selectAdvMapsPreset(1);
+          buyMap(); // try to buy with current settings;
+          if(game.global.mapsOwned == mapsOwnedBeforeFarming) {
+            selectAdvMahpsPreset(2);
+            buyMap();
+            if(game.global.mapsOwned == mapsOwnedBeforeFarming) {
+              selectAdvMahpsPreset(3);
+              buyMap();
+            }
+          }
+          selectMap('map' + game.global.mapsOwned); // Select latest map
+          runMap();
+        }
+      }
+    };
   }
-};
-  
+}
+var autoArg = new AutoArg(); 
 setInterval(new function() {
-  this.autoArgStanceDance();
-  this.autoArgBoneFarm();
+  autoArg.autoArgStanceDance();
+  autoArg.autoArgBoneFarm();
 }, 100); // 100 ms interval
 
 
